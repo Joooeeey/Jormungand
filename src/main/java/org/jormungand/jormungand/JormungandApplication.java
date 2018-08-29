41741d725baa639b10c8455329cdf14deb7cc908
JoeySH <Joey_Shen_struggle@163.com>
2018-08-29 17:36:55 +0800

    Initial commit

---
+++ src/main/java/org/jormungand/jormungand/JormungandApplication.java
@@ -0,0 +1,12 @@
+package org.jormungand.jormungand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JormungandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JormungandApplication.class, args);
+    }
+}
